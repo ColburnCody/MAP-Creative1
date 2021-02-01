56890a6abc49a1998806f44825157054ff3987f0
Cody <ccolburn@uco.edu>
2021-02-01 16:48:54 -0600

    initial commit

--- lib/screen/startscreen.dart
+++ lib/screen/startscreen.dart
@@ -1,20 +1,14 @@
 import 'package:flutter/material.dart';
 
-class StartScreen extends StatefulWidget {
+class StartScreen extends StatelessWidget {
   static const routeName = '/startScreen';
-
-  @override
-  State<StatefulWidget> createState() {
-    return _StartScreen();
-  }
-}
-
-class _StartScreen extends State<StartScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Start Screen'),
+        title: Text(
+          'Start Screen',
+        ),
       ),
     );
   }
